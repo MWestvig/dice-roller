95f2de2680442ad080adaacdeb529cc51eaaa902
Meghan Westvig <m.westvig@gmail.com>
2017-09-18 11:47:54 +0200

    Update dice_roller_simulator.py

--- dice_roller_simulator.py
+++ dice_roller_simulator.py
@@ -1,14 +1,17 @@
 import random
 
 
+# The function to roll the die, it chooses a random number between 1 and 6 and prints it.
 def roll_dice():
     num = random.randint(1, 6)
+    print("Rolling...")
     print(num)
 
 
 print("Here we go! Time to roll the dice.\n")
 rolling = True
 
+# Rolls the die and then asks the user whether they want to roll again, and exits if they do not, or if they enter an unknown answer.
 while rolling:
     roll_dice()
     ans = input("\nRoll Again? (Yes or No): ")
